770f36d503c0c3f701ff67ef05ff52add24dd574
Ajay Parbhane <143879811+Ajju-96@users.noreply.github.com>
2025-01-20 18:08:18 +0530

    first

---
+++ script.js
@@ -0,0 +1,5 @@
+// Form submission alert
+document.querySelector('.contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for getting in touch with us!');
+});
